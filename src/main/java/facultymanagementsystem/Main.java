04e9e2a63d0f9a76f4f3e5c4cf5aa1e92f975502
edpchinthana <edpchinthana@outlook.com>
2020-01-29 13:05:05 +0530

    Initial Commit

---
+++ src/main/java/facultymanagementsystem/Main.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package facultymanagementsystem;
+import GUI.*;
+/**
+ *
+ * @author pasindu
+ */
+public class Main {
+    public static void main(String args[]){
+       Menu menuObject = new Menu();
+       menuObject.setVisible(true);
+    }
+}
---
+++ src/main/java/facultymanagementsystem/Student.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package facultymanagementsystem;
+
+/**
+ *
+ * @author pasindu
+ */
+public class Student {
+    
+    String name;
+    String username;
+    String password;
+    
+    public Student(){
+        
+    }
+    
+}
